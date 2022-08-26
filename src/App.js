60487e370a3cf6c379aac69a29ea5e8dbf04472b
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-08-26 07:13:15 -0400

    added content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import GitHubCard from './GitHubCard/GitHubCard'
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <h2>
+          testing
+        </h2>
       </header>
     </div>
   );
